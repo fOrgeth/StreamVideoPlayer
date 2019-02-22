cbe660b9cf49b504a0a98625ccf797b33a82bfee
fOrgeth <ivdovin@mail.ru>
2019-02-22 20:14:33 +0300

    test video played

--- app/src/main/java/th/forge/streamvideoplayer/MainActivity.java
+++ app/src/main/java/th/forge/streamvideoplayer/MainActivity.java
@@ -1,20 +1,13 @@
 package th.forge.streamvideoplayer;
 
 import android.net.Uri;
-import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
@@ -25,24 +18,23 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-/*import org.videolan.libvlc.MediaPlayer;
-import org.videolan.libvlc.media.VideoView;*/
 
 public class MainActivity extends AppCompatActivity {
-    //    private VideoView videoView;
+
     private PlayerView playerView;
     private SimpleExoPlayer player;
 
     private boolean playWhenReady = true;
     private int currentWindow = 0;
     private long playbackPosition = 0;
-    public static final String URL = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
+    private static final String URL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/hls/TearsOfSteel.m3u8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playerView = findViewById(R.id.video_view);    }
+        playerView = findViewById(R.id.video_view);
+    }
 
     @Override
     protected void onStart() {
@@ -60,10 +52,8 @@ public class MainActivity extends AppCompatActivity {
                 Util.getUserAgent(this, "Stream Video Player"), defaultBandwidthMeter);
 
         Uri uri = Uri.parse(URL);
-        Handler mainHandler = new Handler();
-//        MediaSource mediaSource = new HlsMediaSource(uri, dataSourceFactory, mainHandler, null);
-        MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(uri);
+        MediaSource mediaSource = new HlsMediaSource.Factory(dataSourceFactory)
+               .createMediaSource(uri);
         player.prepare(mediaSource);
         playerView.requestFocus();
         player.setPlayWhenReady(playWhenReady);
